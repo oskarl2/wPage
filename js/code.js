352bea6e93774d1a726c606c1e7be8d1ae32477e
oskarl2 <oskarl2@yandex.ru>
2018-07-12 18:07:19 +0300

    js part 1

--- js/code.js
+++ js/code.js
@@ -1,31 +1,34 @@
 'use strict';
-let body = document.querySelector('body'),
-    submit = document.querySelector('#a'),
+let submit = document.querySelector('#a'),
     modal = document.querySelector('.modal');
+
 submit.style.height = 20 + 'px';
 
 submit.addEventListener('click', showModal);
-document.addEventListener('keyup', closeModal);
 
-// document.addEventListener('mousewheel', horizontalScroll);
-// document.addEventListener('DOMMouseScroll', horizontalScroll);
-
-// function horizontalScroll(e) {
-//     e.preventDefault();
-// }
+function noScroll(e) {
+    if (e.keyCode === 40 || e.keyCode === 38 || e.button === 1 || e.type === 'wheel' ) {
+        e.preventDefault();
+    }
+}
 
 function showModal() {
-    body.classList.add('no-scroll');
-    // modal.addEventListener('mousewheel', horizontalScroll);
-    // modal.addEventListener('DOMMouseScroll', horizontalScroll);
+    modal.style.transition = '2s';
+    modal.style.opacity = '1';
+    document.addEventListener('mousedown', noScroll);
+    document.addEventListener('wheel', noScroll);
+    document.addEventListener('keydown', noScroll);
+    document.addEventListener('keyup', closeModal);
     modal.classList.remove('hidden');
 }
 
 function closeModal(e) {
         if (e.keyCode === 27) {
-            body.classList.remove('no-scroll');
-            // document.removeEventListener('mousewheel', horizontalScroll);
-            // document.removeEventListener('DOMMouseScroll', horizontalScroll);
+            modal.style.transition = 'none';
+            document.removeEventListener('mousedown', noScroll);
+            document.removeEventListener('wheel', noScroll);
+            document.removeEventListener('keydown', noScroll);
+            document.removeEventListener('keyup', closeModal);
             modal.classList.add('hidden');
         }
 }
